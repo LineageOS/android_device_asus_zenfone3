ec6d0c5861759c549c16fc10544725188985b07d
Makornthawat Emery <makornthawateugeneemery@gmail.com>
2018-09-27 21:44:51 +0700

    zenfone3: Use qti bluetooth implementation

--- device.mk
+++ device.mk
@@ -140,12 +140,6 @@ PRODUCT_PACKAGES += \
     FMRadio \
     libfmjni
 
-# Bluetooth
-PRODUCT_PACKAGES += \
-    libbt-vendor \
-    android.hardware.bluetooth@1.0-impl \
-    android.hardware.bluetooth@1.0-service
-
 # Camera
 PRODUCT_PACKAGES += \
     bspcapability \
@@ -466,9 +460,9 @@ PRODUCT_PACKAGES += \
 
 # ANT+
 PRODUCT_PACKAGES += \
+    antradio_app \
     AntHalService \
-    com.dsi.ant.antradio_library \
-    libantradio
+    com.dsi.ant.antradio_library
 
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
