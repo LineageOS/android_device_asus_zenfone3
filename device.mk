48c38a7d1a73b7412a06df6175004c1a06585176
dianlujitao <dianlujitao@lineageos.org>
2018-10-29 18:37:24 +0800

    zenfone3: Remove unused vndk-sp
     * Treble is disabled so VNDK same process libs are useless
    
    Change-Id: I5597419e81692e7b860065422e8015b24475db5b

--- device.mk
+++ device.mk
@@ -478,10 +478,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
-# VNDK-SP:
-PRODUCT_PACKAGES += \
-    vndk-sp
-
 # dataservice
 PRODUCT_PACKAGES += \
     rmnetcli
